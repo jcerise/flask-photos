f0df3d47737d3f734cdff89b29c3b5188b95aab7
jcerise <jcerise06@gmail.com>
2013-03-21 16:36:23 -0600

    Added login/logout, and admin menu (very simple right now)

--- app.py
+++ app.py
@@ -62,5 +62,25 @@ def upload():
            u'<input type="submit" value="Upload">' \
            u'</form>'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
     app.run()
